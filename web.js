068a65db5a544f8ad5923b03a7644bcb814be431
Jacob Friis Saxberg <jacob@saxberg.dk>
2012-03-02 09:39:51 +0100

    should work now, ready for test at Heroku

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = module.exports = express.createServer();
+var clearcf = require(__dirname + '/lib/clearcf.js');
 
 // Configuration
 app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'coffee');
-	app.register('.coffee', require('coffeekup').adapters.express);
+	// app.set('views', __dirname + '/views');
+	// app.set('view engine', 'coffee');
+	// app.register('.coffee', require('coffeekup').adapters.express);
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(express.static(__dirname + '/public'));
+	// app.use(express.static(__dirname + '/public'));
 	app.use(app.router);
 });
 
@@ -25,4 +26,18 @@ app.listen(process.env.PORT || 8008);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 app.get('/', function (req, res) {
+	res.redirect('https://github.com/webjay/clearcf');
+});
+
+app.post('/:distid/:keyaccess/:keysecret', function (req, res) {
+	var distid = req.params.distid;
+	var keyaccess = req.params.keyaccess;
+	var keysecret = req.params.keysecret;
+	var payload = JSON.parse(req.body.payload);
+	var ccf = new clearcf();
+	var files = ccf.parsePayload(payload);
+	var xml = ccf.buildXML(files, payload.after);
+	var date = new Date().toGMTString();
+	var signature = ccf.createSignature(secret_key, date);
+	ccf.sendRequest(distid, date, access_key, signature, xml);
 });
